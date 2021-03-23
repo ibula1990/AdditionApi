518a2868a3709cae870b5aa69916e88e383902c6
lenovo <lenovo@DESKTOP-BLIQA1L>
2021-03-23 00:22:24 -0700

    Add project files.

---
+++ AdditionApi/Controllers/AdditionController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace AdditionApi.Controllers
+{
+    public class AdditionController : ApiController
+    {
+        // GET: Addition
+        public string  GetCalk(int FirstNumber, int SecondNumber)
+        {
+            int c = Convert.ToInt32(FirstNumber) + Convert.ToInt32(SecondNumber);
+            return (c.ToString());
+        }
+    }
+}
---
+++ AdditionApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AdditionApi.Global" Language="C#" %>
